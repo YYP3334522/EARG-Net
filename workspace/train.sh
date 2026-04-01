68bfbcdc21bbcce266cc1c45e31e158f2098ad38
YYP3334522 <113186474+YYP3334522@users.noreply.github.com>
2026-04-01 20:21:20 +0800

    Update training script parameters and paths

--- workspace/train.sh
+++ workspace/train.sh
@@ -3,8 +3,8 @@ mkdir -p "${base_dir}"
 
 GPU_ID=${GPU_ID:-0}
 MODEL_NAME=${MODEL_NAME:-EARG}
-DATA_PATH=${DATA_PATH:-/data/yuyanpu/dataset/CASIA1.0}
-TEST_DATA_PATH=${TEST_DATA_PATH:-/data/yuyanpu/dataset/CASIA1.0}
+DATA_PATH=${DATA_PATH:-./balanced_dataset.json}
+TEST_DATA_PATH=${TEST_DATA_PATH:-./validation_dataset.json}
 EARG_SD_MODEL_PATH=${EARG_SD_MODEL_PATH:-/data/yuyanpu/model/SD/stable-diffusion-2-inpainting}
 
 export EARG_SD_MODEL_PATH
@@ -16,21 +16,20 @@ torchrun \
     ./train.py \
     --model "${MODEL_NAME}" \
     --world_size 1 \
-    --batch_size 1 \
+    --batch_size 8 \
     --data_path "${DATA_PATH}" \
-    --epochs 200 \
+    --epochs 50 \
     --lr 1e-4 \
     --image_size 512 \
     --if_resizing \
     --find_unused_parameters \
     --min_lr 5e-7 \
     --weight_decay 0.05 \
-    --edge_mask_width 7 \
     --test_data_path "${TEST_DATA_PATH}" \
     --warmup_epochs 2 \
     --output_dir "${base_dir}/" \
     --log_dir "${base_dir}/" \
     --accum_iter 8 \
     --seed 42 \
-    --test_period 4 \
+    --test_period 10 \
     2> "${base_dir}/error.log" 1> "${base_dir}/logs.log"
